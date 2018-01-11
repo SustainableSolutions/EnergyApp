030d4765857814cb1d99cc0dce2458553213b031
Melissa Doss <mdoss@NSR.LOCAL>
2018-01-10 19:06:10 -0500

    Created React Native project.

---
+++ src/firebase.js
@@ -0,0 +1,21 @@
+import * as firebase from 'firebase';
+
+const config = {
+    
+<script src="https://www.gstatic.com/firebasejs/4.8.1/firebase.js"></script>
+<script>
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyAzrkkUdfQalJ56ma-69vJZZd-RMuqtAAU",
+    authDomain: "energyapp-79b0a.firebaseapp.com",
+    databaseURL: "https://energyapp-79b0a.firebaseio.com",
+    projectId: "energyapp-79b0a",
+    storageBucket: "energyapp-79b0a.appspot.com",
+    messagingSenderId: "973911986602"
+  };
+  firebase.initializeApp(config);
+</script>
+};
+firebase.initializeApp(config);
+
+export default firebase;
